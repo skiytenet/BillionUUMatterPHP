bfd821bd09bf69ffee0a9dd2dacbdc6015a592f9
skiytenet <skiyte.gaming@gmail.com>
2015-10-28 10:54:07 +0100

    Update updatefluids.php

--- updatefluids.php
+++ updatefluids.php
@@ -2,7 +2,7 @@
 
 if(isset($_POST['fluid'])&&isset($_POST['amount'])&&isset($_POST['capacity']))
 {
-  $fluids = json_decode(file_POST_contents("array.json"), true);
+  $fluids = json_decode(file_get_contents("array.json"), true);
   $fluids[$_POST['fluid']]['name'] = $_POST['fluid'];
   $fluids[$_POST['fluid']]['amount'] = $_POST['amount'];
   $fluids[$_POST['fluid']]['capacity'] = $_POST['capacity'];
